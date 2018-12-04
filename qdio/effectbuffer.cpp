771490b0b3232b9aa3d1ef674aaba4c34e746793
Maxime445 <maxrofra@gmail.com>
2018-12-04 16:58:17 +0000

    Added effectbuffer class and some implementation. Unsuccessful due to "Open Error, invalid format" for both QAudioInput and QAudioOutput implementations. devices class correctly implemented, some updates on ui.

---
+++ qdio/effectbuffer.cpp
@@ -0,0 +1,6 @@
+#include "effectbuffer.h"
+
+EffectBuffer::EffectBuffer()
+{
+
+}
---
+++ qdio/effectbuffer.h
@@ -0,0 +1,12 @@
+#ifndef EFFECTBUFFER_H
+#define EFFECTBUFFER_H
+
+#include <QObject>
+
+class EffectBuffer : public QIODevice
+{
+public:
+    EffectBuffer();
+};
+
+#endif // EFFECTBUFFER_H
